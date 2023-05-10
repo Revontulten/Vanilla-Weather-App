deb6cf8b0951889d9cd8bbe6bf61778b477cff29
Olha Kolodiazhna <92469739+Revontulten@users.noreply.github.com>
2023-05-10 19:51:59 +0100

    Update app.js + the forecast with the fake data

--- src/app.js
+++ src/app.js
@@ -21,6 +21,37 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Wed", "Thu", "Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+              <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                
+                <img
+                  src="https://openweathermap.org/img/wn/10d@2x.png"
+                  alt=""
+                  width="42"
+                />
+                <div class="weather-forecast-temperature">
+                  <span class="weather-forecast-temp-max">18° </span>
+                  <span class="weather-forecast-temp-min">12°</span>
+                </div>
+              </div>`;
+  });
+
+  forecastHTML =
+    forecastHTML +
+    `
+              </div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
@@ -87,4 +118,4 @@ let celciusLink = document.querySelector("#celcius-link");
 celciusLink.addEventListener("click", displayCelciusTemp);
 
 search("Dublin");
-
+displayForecast();
